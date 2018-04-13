25922a6c9328bf44bfb994c0c79ae148ef55a88a
Dylan Wellner <dylan.wellner@student.uva.nl>
2018-04-13 15:01:48 +0000

    Extra move functie

--- vehicle.py
+++ vehicle.py
@@ -8,3 +8,11 @@ class vehicle:
         self.yBegin = yBegin
         self.length = length
         self.orientation = orientation
+
+    # move orientation coord plus or minus 1
+    def move(self, direction):
+
+        if self.orientation == 'H':
+            self.xBegin + direction
+        elif self.orientation == 'V':
+            self.yBegin + direction
