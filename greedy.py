6408257b0d127b8f8265fe2c979524dd4923d81d
Jacco van Wijk <jaccov.wijk@yahoo.com>
2018-04-17 07:01:09 +0000

    greedy los bestand

---
+++ greedy.py
@@ -0,0 +1,32 @@
+def solve(vehicle):
+
+    direction = vehicle.orientation
+    if direction == "H":
+        # pick everything in it's row
+        possibleDrive = game.initBoard[vehicle.yBegin - 1]
+    elif direction == "V":
+        # pick everything in it's column
+        possibleDrive = game.initBoard[vehicle.xBegin::game.size]
+
+    moved = 0
+    while moved < vehicle.length: #deze voorwaarde moet nog beter
+
+        # keep track of position relative to goalcar
+        afterGoal = False
+        for letter in possibleDrive:
+            if letter == vehicle.name:
+                afterGoal = True
+            elif letter == ".":
+                if afterGoal:
+                    begin = [vehicle.xBegin,vehicle.yBegin]
+                    vehicle.moveHistory.append(begin)
+                    vehicle.move(1)
+            else:
+                # if there's a car in front move it
+                for car in game.vehicles:
+                    if car.name == letter and afterGoal:
+                        solve(car)
+
+                begin = [vehicle.xBegin,vehicle.yBegin]
+                vehicle.moveHistory.append(begin)
+                vehicle.move(1)
