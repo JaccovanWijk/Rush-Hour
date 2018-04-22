fb907fc6114299c276d20f39796d93aa208da266
JaccovanWijk <jaccov.wijk@yahoo.com>
2018-04-22 16:22:41 +0200

    test commit

--- greedy.py
+++ greedy.py
@@ -4,12 +4,12 @@ class greedy(r.rushHour):
     
     def __init__(self, board):
         
-        r.rushHour.__init__(board)
+        r.rushHour.__init__(self,board)
         
         
     def solve(self, board):
         
-        print(self.initBoard)
+        print(self.initBoard, 1)
         for car in self.vehicles:
                 if car.name == "X":
                     currentCar = car
@@ -18,19 +18,22 @@ class greedy(r.rushHour):
         testedVehicles = []
         move = 0
         while move < 3:#not self.won():
-            
+            print(self.initBoard, 2)
             # zorg dat deze de maximale moves naar beide kanten wordt
             possibleMoves = self.searchMoves(currentCar)
             x = max(s for s in possibleMoves)
             y = min(s for s in possibleMoves)
             moves = [x,y]
-            
+            print(self.initBoard,3)
             if currentCar not in testedVehicles:
                 goalVehicles.append(currentCar)
                 testedVehicles.append(currentCar)
-                
+            #elif len(moves) > 0:
+            #    moves.pop(0)
+            #else:
+            print(self.initBoard,4)
             if len(moves) == 0:
-                
+                print(self.initBoard,5)
                 neighbours = self.neighboursFinder(currentCar)
                 
                 currentCar = neighbours[0]
@@ -38,15 +41,15 @@ class greedy(r.rushHour):
                 #currentCar = goalVehicles[-2]
                 #goalVehicles.pop()
             elif len(moves) == 1:
-                
-            
+                self.makingMove(currentCar, moves[0])
+                print(self.initBoard,6)
             else:
                 # take first argument of moves
                 self.makingMove(currentCar,moves[0])
-                
+                print(self.initBoard,7)
+            print(self.initBoard, 8)
 
             move += 1
-        print(self.initBoard)
         
         
     def neighboursFinder(self, vehicle):
