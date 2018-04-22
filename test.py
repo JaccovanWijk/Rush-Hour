288717d4b645cf239268bc9b5feb5461ae3168c6
deal10041 <31853100+deal10041@users.noreply.github.com>
2018-04-22 15:41:02 +0200

    "merge"

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
+<<<<<<< current
 import rushHour as r
 import breadthFirst as br
-import greedy as gr
 
 f = open("Boards/Game 1", "r")
 board = f.read()
@@ -10,4 +10,20 @@ f.close()
 game = br.breadthFirst(board)
 
 #moves = game.breadthFirstSearch()
-#print(moves)
+#print(moves)#print(moves)moves = game.breadthFirstSearch()
+print(moves)
+=======
+import rushHour as r
+import breadthFirst as br
+
+f = open("Boards/Game 1", "r")
+board = f.read()
+f.close()
+
+
+game = br.breadthFirst(board)
+
+#moves = game.breadthFirstSearch()
+#print(moves)moves = game.breadthFirstSearch()
+print(moves)
+>>>>>>> before discard
