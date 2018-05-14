90e46ac8cbe8076086408582aecf04d9bd8dc08f
Ian Epping <medigoultima@gmail.com>
2018-05-14 12:20:15 +0000

    vervangen versie van A*

--- code/algoritmes/Astar.py
+++ code/algoritmes/Astar.py
@@ -1,41 +1,157 @@
-
 import rushHour as r
 import vehicle as v
 from time import time
+from copy import deepcopy
 import heapq
 
 class PriorityQueue:
     def __init__(self):
-        self.elements = []
+        self._queue = []
+        self._index = 0
+
+    def push(self, item, priority):
+        heapq.heappush(self._queue, (-priority, self._index, item))
+        self._index += 1
 
     def empty(self):
-        return len(self.elements) == 0
+        return len(self._queue) == 0
 
-    def put(self, item, priority):
-        heapq.heappush(self.elements, (priority, item))
+    def pop(self):
+        return heapq.heappop(self._queue)[-1]
 
-    def get(self):
-        return heapq.heappop(self.elements)[1]
+class ZeroHeuristic:
+    """Zero Heuristcs, same as a Breadth First search"""
+
+    def calculate(self, board):
+        return 0
+
+    def __repr__(self):
+        return 'ZeroHeuristic'
+
+class BlockingCarsHeuristic:
+
+    #Calculate the number of blocking vehicles
+    def obstruction(self, board):
+        red_vehicle = [
+            vehicle for vehicle in board.vehicles if vehicle.name == 'X'][0]
+        if red_vehicle.coord['x'] == 4:
+            return 0
+        blockingvehicles = 1
+        for vehicle in board.vehicles:
+            if vehicle.orientation == "V" and vehicle.coord['x'] >= (red_vehicle.coord['x']
+            + red_vehicle.length) and (vehicle.coord['y'] <= red_vehicle.coord['y']
+            and vehicle.coord['y'] + vehicle.length > red_vehicle.coord['y']):
+                blockingvehicles += 1
+        return blockingvehicles
+
+    def __repr__(self):
+        return 'BlockingCarsHeuristic'
 
 class A_Star(r.RushHour):
     """"An A* search algorithm for Rush Hour"""
 
-    def __init__(self, board, size):
+    def __init__(self, board, heuristic, size):
 
-        r.RushHour.__init__(self,board, size)
+        r.RushHour.__init__(board, heuristic)
 
         # model game
+        self.board = board
+        self.heuristic = heuristic
         self.currentBoard = self.initBoard
         self.currentVehicles = self.vehicles
-        #self.board = board
-        #self.size = size
-        self.parent = None
+        self.size = size
+        #self.parent = None
+        #self.map = []
+
+    # the searching algorithm
+    def solver(self):
+
+        # open and closed possibilities
+        openBoards = PriorityQueue()
+        board = self.board
+        closedBoards = set()
+        # moves done
+        moves = dict()
 
         # cost of path from start to end-nodes
-        self.G_Cost = {}
+        G_Cost = {}
 
         # estimated cost of the cheapest path to goal
-        self.H_Cost = {}
+        #H_Cost = {}
+
+        # iteration counter
+        count = 0
+
+        # returns solution, amount of moves, G_Cost and iterations
+        beginTime = time()
+
+        # initialise search, move count and cost
+        self.openBoards.insert(0, self.currentBoard)
+        self.moves[self.currentBoard] = ()
+        self.G_Cost[self.currentBoard] = 0
+
+        while not openBoards.empty():
+            currentBoard = openBoards.pop()
+            #newBoard = self.Board()
+
+            # update cars
+            self.currentVehicles = self.getVehicles(self.currentBoard)
+
+            # stop if puzzle is goal is met
+            if self.won(self.currentVehicles):
+                return (self.showMoves(self.currentBoard, moves), count, time() - beginTime)
+            count += 1
+
+            # loop through the newBoard's sucessors/siblings
+            for newBoard in self.getSucessors(currentBoard):
+
+                # if it is already in the closed set, skip it
+                if newBoard in closedBoards:
+                    continue
+
+                # otherwise if it is already in the queue
+                if newBoard in self.openBoards:
+
+                    # Check if we beat the cost and update if so.
+                    new_cost = currentBoard.G_Cost + board.cost(currentBoard, newBoard)
+                    priority = new_cost + self.heuristic.obstruction(newBoard)
+                    if newBoard not in self.G_Cost or new_cost < newBoard.G_Cost:
+                        newBoard.G_Cost = new_cost
+                        openBoards.push(next, priority)
+                        #closedBoards.add(currentBoard)
+                        newBoard.parent = currentBoard
+
+                else:
+                    # if it isn't in the open board yet
+                    newBoard.G_Cost = currentBoard.G_Cost + currentBoard.cost(newBoard)
+                    #newBoard.H_Cost = self.obstruction(newBoard, won)
+
+                    # set the parent to our currentBoard item
+                    newBoard.parent = currentBoard
+
+                    # add it to the queue
+                    newBoard = openBoards.pop()
+
+            # move the item from the queue and add to the closed set
+            currentBoard = openBoards.empty()
+            closedBoards.add(currentBoard)
+
+            # check for errors
+            raise ValueError('Not Solvable')
+
+        # step by step guide of all moves used to solve the puzzle
+        def solution(self, board):
+            output = ''
+            output += "; ".join(["{} {}".format(move[0], move[1]) for move in moves])
+            vehicles = deepcopy(board.vehicles)
+            for move in moves:
+                vehicle = [x for x in vehicles if x.vehicles == move[0]][0]
+                output += '\nMOVE {} {}\n'.format(move[0], move[1])
+                vehicle.move(move[1], 1)
+                output += self.board.prettify(vehicles)
+            return output
+
+
 
     def getSucessors(self, point):
         """Get next board states reachable by making one move"""
@@ -54,88 +170,3 @@ class A_Star(r.RushHour):
                 sucessors.append([newBoard, move])
 
         return sucessors
-
-    # heuristic function
-    def distance(self, point1, point2):
-        (x1, y1) = point1
-        (x2, y2) = point2
-        return abs(x1 - x2) + abs(y1 - y2)
-
-    def solver(self, board, start, won):
-
-        # open and closed possibilities
-        openBoards = PriorityQueue()
-        closedBoards = set()
-        # moves done
-        moves = dict()
-
-        # iteration counter
-        count = 0
-        beginTime = time()
-
-        # initialise search and cost
-        self.currentBoard = start
-        self.G_Cost[start] = 0
-
-        while not openBoards.empty():
-            currentBoard = openBoards.get()
-            #newBoard = self.Board()
-
-            # update cars
-            self.currentVehicles = self.getVehicles(self.currentBoard)
-
-            # stop if puzzle is goal is met
-            if self.won(self.currentVehicles):
-                return (self.showMoves(self.currentBoard, moves), count, time() - beginTime)
-            #if currentBoard == won:
-                #break
-
-            count += 1
-
-            # loop through the newBoard's sucessors/siblings
-            for newBoard in self.getSucessors(currentBoard):
-
-                # if it is already in the closed set, skip it
-                if newBoard in closedBoards:
-                    continue
-
-                # otherwise if it is already in the queue
-                if newBoard in self.openBoards:
-
-                    # Check if we beat the cost and update if so.
-                    new_cost = currentBoard.G_Cost + board.cost(currentBoard, newBoard)
-                    if newBoard not in self.G_Cost or new_cost < newBoard.G_Cost:
-                        newBoard.G_Cost = new_cost
-                        priority = new_cost + self.distance(won, next)
-                        openBoards.put(next, priority)
-                        newBoard.parent = currentBoard
-
-                else:
-                    # if it isn't in the open board yet
-                    newBoard.G_Cost = currentBoard.G_Cost + currentBoard.cost(newBoard)
-                    newBoard.H_Cost = self.distance(newBoard, won)
-
-                    # set the parent to our currentBoard item
-                    newBoard.parent = currentBoard
-
-                    # add it to the queue
-                    newBoard = openBoards.get()
-
-            # move the item from the queue and add to the closed set
-            currentBoard = openBoards.empty()
-            closedBoards.add(currentBoard)
-
-            # resulting costs of moves
-            #return self.parent, self.G_Cost
-            raise ValueError('No Path Found')
-
-        # recount the path of all moves
-        def reconstruct_path(G_Cost, start, won):
-            currentBoard = won
-            path = []
-            while currentBoard != start:
-                path.append(currentBoard)
-                currentBoard = self.G_Cost[currentBoard]
-            # path.append(start)
-            # path.reverse()
-            return path
