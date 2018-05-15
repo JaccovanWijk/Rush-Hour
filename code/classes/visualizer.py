a268d9717b0e7eaf429d98ccfab621a90fb9a64e
deal10041 <31853100+deal10041@users.noreply.github.com>
2018-05-15 11:16:47 +0200

    Dawn of a new Visualization

--- code/classes/visualizer.py
+++ code/classes/visualizer.py
@@ -1,12 +1,12 @@
 import pygame
 import random
 
-def drawBoard(vehicles, size):
+def drawBoard(vehicles, size, huemap):
 
     # initialise image
     image = pygame.Surface((size * 50, size * 50))
     image.fill((127, 127, 127))
-    huemap = readBoard(vehicles)
+    WHITE = (255,255,255)
 
     # draw all cars
     for i in range(len(vehicles)):
@@ -18,8 +18,37 @@ def drawBoard(vehicles, size):
             rect = [vehicle.xBegin * 50 + 5, vehicle.yBegin * 50 + 5, 40, 50 * vehicle.length - 10]
         image.fill(huemap[vehicle.name], rect)
 
+        if vehicle.name == 'X':
+            # corners of goal car
+            nw = [vehicle.xBegin * 50 + 5, vehicle.yBegin * 50 + 5]
+            ne = [(vehicle.xBegin + vehicle.length) * 50 - 6, vehicle.yBegin * 50 + 5]
+            sw = [vehicle.xBegin * 50 + 5, vehicle.yBegin * 50 + 43]
+            se = [(vehicle.xBegin + vehicle.length) * 50 - 6, vehicle.yBegin * 50 + 43]
+
+            # draw a cross
+            pygame.draw.line(image, WHITE, nw, se, 2)
+            pygame.draw.line(image, WHITE, ne, sw, 2)
+
     # save image
-    pygame.image.save(image, "drawing.jpeg")
+    pygame.image.save(image, "drawing.png")
+
+
+def readBoard(vehicles):
+    """Read in board and make hue map"""
+
+    # read in all unique names
+    names = [vehicle.name for vehicle in vehicles]
+    nameCount = len(names) - 1
+    # move goal car to the front
+    random.shuffle(names)
+    names.remove('X')
+
+    # make huemap
+    huemap = dict()
+    for i in range(nameCount):
+        huemap[names[i]] = (hue(i / nameCount))
+    huemap['X'] = ((255,0,0))
+    return huemap
 
 def hue(number):
     """Returns a hue for a given number between 0 and 1"""
@@ -39,20 +68,3 @@ def hue(number):
     # Purple to red
     else:
         return (255, 0, 255 * (1 - (number-5/6) * 6))
-
-def readBoard(vehicles):
-    """Read in board and make hue map"""
-
-    # read in all unique names
-    names = [vehicle.name for vehicle in vehicles]
-    nameCount = len(names) - 1
-    # move goal car to the front
-    random.shuffle(names)
-    names.remove('X')
-
-    # make huemap
-    huemap = dict()
-    for i in range(nameCount):
-        huemap[names[i]] = (hue(i / nameCount))
-    huemap['X'] = ((255,0,0))
-    return huemap
