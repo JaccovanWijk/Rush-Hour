db5af0e599a7d0a08911befd6ebdf467217f1031
JaccovanWijk <jaccov.wijk@yahoo.com>
2018-05-17 13:08:12 +0200

    Merge?

--- code/classes/visualizer.py
+++ code/classes/visualizer.py
@@ -11,11 +11,11 @@ def drawBoard(vehicles, size, huemap, name):
 
     # initialise image
     image = pygame.Surface((size * 500, size * 500))
-
     image.fill(GREY)
 
     # draw all cars
     for vehicle in vehicles:
+
         drawRect(image, huemap, vehicle)
 
         if vehicle.name == 'X':
@@ -58,4 +58,37 @@ def drawRect(image, huemap, vehicle):
     image.fill(huemap[vehicle.name], rect)
 
 def readBoard(vehicles):
-     """Read in board and make hue map"""
+    """Read in board and make hue map"""
+
+    # read in all unique names
+    names = [vehicle.name for vehicle in vehicles]
+    nameCount = len(names) - 1
+    # move goal car to the front
+    random.shuffle(names)
+    names.remove('X')
+
+    # make huemap
+    huemap = dict()
+    for i in range(nameCount):
+        huemap[names[i]] = (hue(i / nameCount))
+    huemap['X'] = ((255,0,0))
+    return huemap
+
+def hue(number):
+    """Returns a hue for a given number between 0 and 1"""
+
+    # Orange to green
+    if number < 2/6:
+        return (255 * (1 - (number) * 3), 255, 0)
+    # Green to cyan
+    elif number < 3/6:
+        return (0, 255, 255 * (number-2/6) * 6)
+    # Cyan to blue
+    elif number < 4/6:
+        return (0, 255 * (1 - (number-3/6) * 6), 255)
+    # Blue to purple
+    elif number < 5/6:
+        return (255 * (number-4/6), 0, 255)
+    # Purple to red
+    else:
+        return (255, 0, 255 * (1 - (number-5/6) * 6))
