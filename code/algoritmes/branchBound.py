c9592cb912a09d7a4dd286799fe26fc20abb3c82
JaccovanWijk <jaccov.wijk@yahoo.com>
2018-05-22 20:30:32 +0200

    Astar3:)

--- code/algoritmes/branchBound.py
+++ code/algoritmes/branchBound.py
@@ -149,25 +149,3 @@ class BranchBound(r.RushHour):
         #
         # scores.append((score, (board, i)))
         return score
-
-# class PriorityQueue(Queue.Queue):
-#
-#     def _insert(self, item):
-#         board, score = item
-#         self.place((board, score))
-#
-#     def _get(self):
-#         return self.queue.pop(0)[1]
-#
-#     def place(self, item):
-#         openBoards = self.queue
-#         low = 0
-#         high = len(openBoards)
-#
-#         while low < high:
-#             mid = (low+high)/2
-#             if item[0] < openBoards[mid][0]:
-#                 high = mid
-#             else:
-#                 low = mid + 1
-#         openBoards.insert(low, item)
