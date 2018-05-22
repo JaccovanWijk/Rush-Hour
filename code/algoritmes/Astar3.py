9bf87547f22c176de6c0c154364a0018d99f3d07
JaccovanWijk <jaccov.wijk@yahoo.com>
2018-05-22 21:43:14 +0200

    Astar3

--- code/algoritmes/Astar3.py
+++ code/algoritmes/Astar3.py
@@ -1,6 +1,6 @@
 import rushHour as r
 import BruteForce as bf
-import queue
+import queue as Qu
 
 class aStar(r.RushHour):
 
@@ -8,34 +8,77 @@ class aStar(r.RushHour):
 
         r.RushHour.__init__(self, board)
         self.currentBoard = board
-        self.openBoards = set()
         self.closedBoards = set()
-        self.queue =
-
-    def solver(self)
-
-
+        self.priorityQueue = Qu.PriorityQueue()
+        self.moves = dict()
+        self.count = 0
 
 
+    def aStarSolve(self):
 
-class PriorityQueue(Queue.Queue):
+        self.priorityQueue.put((self.currentBoard, 0))
 
-    def _insert(self, item):
-        board, score = item
-        self.place((board, score))
+        self.moves[self.currentBoard] = ()
 
-    def _get(self):
-        return self.queue.pop(0)[1]
+        while not self.priorityQueue.empty():
 
-    def place(self, item):
-        openBoards = self.queue
-        low = 0
-        high = len(openBoards)
+            self.currentBoard = self.priorityQueue.get()
+            self.closedBoards.add(self.currentBoard)
 
-        while low < high:
-            mid = (low+high)/2
-            if item[0] < openBoards[mid][0]:
-                high = mid
-            else:
-                low = mid + 1
-        openBoards.insert(low, item)
+            self.currentVehicles = self.getVehicles(self.currentBoard)
+
+            if self.won(self.currentVehicles):
+                break
+
+            self.count += 1
+
+            for newBoard in self.getSucessors():
+
+                if newBoard in self.closedBoards:
+                    continue
+
+                self.moves[newBoard] = (self.currentBoard)
+
+                score = 0
+                self.priorityQueue.put((score, newBoard))
+
+
+
+    def getSucessors(self):
+        """Get next board states reachable by making one move"""
+        sucessors = []
+        cars = self.currentVehicles
+
+        # get all moves of all vehicles
+        for vehicle in self.currentVehicles:
+            for i in self.searchMoves(self.currentBoard, vehicle):
+                # determine new state
+                newBoard = self.makingMove(self.currentVehicles,vehicle, i)
+                self.makingMove(self.currentVehicles,vehicle, -i)
+                move = vehicle.name + " " + str(i)
+
+                sucessors.append(newBoard)
+
+        return sucessors
+
+
+# class PriorityQueue(Queue.Queue):
+#
+#     def _put(self, item):
+#         self.place(item)
+#
+#     def _get(self):
+#         return self.queue.pop(0)[1]
+#
+#     def place(self, item):
+#         openBoards = self.queue
+#         low = 0
+#         high = len(openBoards)
+#
+#         while low < high:
+#             mid = (low+high)/2
+#             if item[0] < openBoards[mid][0]:
+#                 high = mid
+#             else:
+#                 low = mid + 1
+#         openBoards.insert(low, item)
