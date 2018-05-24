326c65cfd1ca7cac876013c90bc729cd277e322f
JaccovanWijk <jaccov.wijk@yahoo.com>
2018-05-24 16:43:17 +0200

    main

--- main.py
+++ main.py
@@ -11,33 +11,74 @@ import Astar as A
 import breadthFirst as br
 import BruteForce as bf
 
+currentAlgorithm = ""
+amount = 0
+
 class Application(Frame):
-    def say_hi(self):
-        print ("hi there, everyone!")
+    def nextPage(self, algorithm):
+        currentAlgorithm = algorithm
+        self.quit
 
     def createWidgets(self):
-        self.QUIT = Button(self)
-        self.QUIT["text"] = "QUIT"
-        self.QUIT["fg"]   = "red"
-        self.QUIT["command"] =  self.quit
+        # make text
+        self.welcome = Label(self, text="Which algorithm would you like to use?")
+        self.welcome.pack()
 
-        self.QUIT.pack({"side": "left"})
+        # make random algorithm button
+        self.randomAlg = Button(self)
+        self.randomAlg["text"] = "Random Algorithm",
+        self.randomAlg["command"] = lambda: self.nextPage("Random")
 
-        self.hi_there = Button(self)
-        self.hi_there["text"] = "Hello",
-        self.hi_there["command"] = self.say_hi
+        self.randomAlg.pack(pady=5)
 
-        self.hi_there.pack({"side": "left"})
+        # make breadth first button
+        self.BreadthFirstAlg = Button(self)
+        self.BreadthFirstAlg["text"] = "Breadth First Algorithm",
+        self.BreadthFirstAlg["command"] = lambda: self.nextPage("BreadthFirst")
+
+        self.BreadthFirstAlg.pack(pady=5)
+
+        # make astar button
+        self.AstarAlg = Button(self)
+        self.AstarAlg["text"] = "Astar Algorithm",
+        self.AstarAlg["command"] = lambda: self.nextPage("Astar")
+
+        self.AstarAlg.pack(pady=5)
+
+        # make branch and bound button
+        self.BranchBoundAlg = Button(self)
+        self.BranchBoundAlg["text"] = "Branch and Bound Algorithm",
+        self.BranchBoundAlg["command"] = lambda: self.nextPage("BranchandBound")
+
+        self.BranchBoundAlg.pack(pady=5)
 
     def __init__(self, master=None):
         Frame.__init__(self, master)
+        self.label = Label(self, text="Menu")
         self.pack()
         self.createWidgets()
+
+def printer(message):
+    print(message)
+
+class AmountPage(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        self.label = Label(self, text="Menu")
+        self.pack()
+
 
 def main():
 
     root = Tk()
     app = Application(root)
+    app.mainloop()
+    root.destroy()
+
+    print(currentAlgorithm)
+
+    root = Tk()
+    app = AmountPage(root)
     app.mainloop()
     root.destroy()
 
@@ -54,4 +95,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    print("HOi")
