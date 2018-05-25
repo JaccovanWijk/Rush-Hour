cacea629c2f155c635f138086d8b3cdf2c9f4abe
deal10041 <31853100+deal10041@users.noreply.github.com>
2018-05-25 22:09:11 +0200

    Merge branch 'master' of https://github.com/JaccovanWijk/Rush-Hour

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from tkinter import ttk
 directory = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(directory, "code"))
 sys.path.append(os.path.join(directory, "code", "classes"))
-sys.path.append(os.path.join(directory, "code", "algoritmes"))
+sys.path.append(os.path.join(directory, "code", "algorithms"))
 
 # import matplotlib
 # matplotlib.use("TkAgg")
